a9c3a11384760eb6ccf2ae5560dd49f1d190eaac
Andrew Moses <andrewmosesdrive@gmail.com>
2020-12-12 14:40:46 -0700

    deploying

--- Develop/server.js
+++ Develop/server.js
@@ -18,6 +18,8 @@ app.use(express.static("public"));
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 app.get("/api/workouts", (req, res) => {
